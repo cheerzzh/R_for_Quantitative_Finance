165be5ef6af75a3628f291b9a936bd5911bbe836
Jared Zhou <cheerzzh@gmail.com>
2014-07-25 10:12:45 +0800

    convert target tenor in a day

--- rate-analysis/spline-interpolation.R
+++ rate-analysis/spline-interpolation.R
@@ -34,14 +34,37 @@ contour(akima.bic, add=TRUE)
 # 
 
 t[100,2]
+t[446,9]
+
 d <- data.frame(i=rep(1:nrow(t),ncol(t)),
                 j=rep(1:ncol(t),each=nrow(t)),
                 rate=as.vector(t))
 
 
-d[d$i == 100 & d$j ==2,3]
+d[d$i == 100 & d$j ==2,3] # to test whether the 
 removeList <- d$i == 100 & d$j ==2
+removeList <- removeList | (d$i==446 & d$j==9)
 temp <- d[-removeList,]
 # subset(d , i==100 & j ==2)
+d[c(100,120,143,456),]
+temp <- d[-c(100,120,143,456),]
+
+# a procedure to convert specific date into 2 index for d
+# 2010-6-9 X9M -> t[i,j] -> d[n,]
+
+dateIndex <- index(t)
+targetDate <- as.POSIXct("2009-06-02")
+index1 <- which(dateIndex == target)
+tenorName <- names(t)
+targetTenor <- "X6M"
+index2 <- which(tenorName == targetTenor)
+
+t[index1,index2]
+
+# remove the target tennor in a day
 
 li <- with(temp,interp(i, j,rate,xo=100,yo=2))
+
+
+
+
